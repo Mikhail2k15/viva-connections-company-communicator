20285cbf58507589ab7deb3995724601a0d0fcb4
Michael Bondarevsky <bondarevsky@gmail.com>
2022-04-06 14:52:18 +0000

    Add Insights Web Part

---
+++ src/webparts/companyCommunicatorInsights/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface ICompanyCommunicatorInsightsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'CompanyCommunicatorInsightsWebPartStrings' {
+  const strings: ICompanyCommunicatorInsightsWebPartStrings;
+  export = strings;
+}
